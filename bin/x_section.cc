3c88c35115c19544fb5c2dbe8eadac6683f47026
EV 19 Alexandra Pardal <ev19u032@fermi04.ncg.ingrid.pt>
2019-09-03 12:06:45 +0100

    updated

---
+++ bin/x_section.cc
@@ -0,0 +1,74 @@
+/////////////////////////////////////////////////////////////////////////
+//
+// X-section, ratio and systematics
+//
+//
+// August 2019
+//
+/////////////////////////////////////////////////////////////////////////
+
+
+#include <sstream>
+#include <vector>
+#include <TStyle.h>
+#include <TAxis.h>
+#include <TLatex.h>
+#include <TPaveText.h>
+#include <TFile.h>
+#include <TTree.h>
+#include <TString.h>
+#include <TChain.h>
+#include <TCanvas.h>
+#include <TNtupleD.h>
+#include <TH1D.h>
+#include <TLorentzVector.h>
+#include <TLegend.h>
+#include <TSystem.h>
+#include <RooWorkspace.h>
+#include <RooRealVar.h>
+#include <RooConstVar.h>
+#include <RooDataSet.h>
+#include <RooGaussian.h>
+#include <RooChebychev.h>
+#include <RooBernstein.h>
+#include <RooExponential.h>
+#include <RooAddPdf.h>
+#include <RooPlot.h>
+#include <RooBifurGauss.h>
+#include "TMath.h"
+#include <RooGenericPdf.h>
+#include "TRatioPlot.h"
+#include <RooBifurGauss.h>
+#include <RooProduct.h>
+#include <RooHist.h>
+#include "RooStats/SPlot.h"
+#include "RooProdPdf.h"
+#include "RooAddition.h"
+#include "RooProduct.h"
+#include "RooAbsPdf.h"
+#include "RooFit.h"
+#include "RooFitResult.h"
+#include "RooMCStudy.h"
+#include <iostream>
+#include <TF1.h>
+#include <RooPolynomial.h>
+#include <fstream>
+using namespace RooStats;
+using namespace RooFit;
+using namespace std;
+
+//functions
+
+//particle
+// 0 = Bu
+// 1 = Bs
+
+#define particle 0
+
+int main(){
+
+  TString input_raw_yield = particle ? "./results/Bs/Bpt/pT.root" : "./results/Bu/Bpt/pT.root";
+  TString input_efficiency = particle ? //colocar os outputs do joão
+
+const int branching_fraction = particle ? 
+const int luminosity = 
